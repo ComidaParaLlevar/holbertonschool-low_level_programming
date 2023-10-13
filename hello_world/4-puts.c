a24dc3ac46bf9f99f855fbe8c248dc9a37cf782f
comidaparallevar <lyanosorio98@gmail.com>
2023-10-13 14:12:05 -0400

    deletion errors

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
-int main (void) 
+int main(void)
 {
 	char string[] = "\"Programming is like building a multilingual puzzle\n";
-	
+
 	puts(string);
 
-	return(0);	
+	return(0);
 }
 

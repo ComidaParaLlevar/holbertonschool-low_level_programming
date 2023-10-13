090b59bac7881c3394d3facffaa857768b9b25b7
comidaparallevar <lyanosorio98@gmail.com>
2023-10-13 14:07:23 -0400

    added "

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,7 +3,7 @@
 
 int main (void) 
 {
-	char string[] = "Programming is like building a multilingual puzzle\n";
+	char string[] = "\"Programming is like building a multilingual puzzle\n";
 	
 	puts(string);
 

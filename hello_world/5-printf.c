f35435e161bcc6e6a8a51aba10893105492900ba
comidaparallevar <lyanosorio98@gmail.com>
2023-10-13 16:12:19 -0400

    added 5-printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ *main - printf string
+ *Return: 0
+ */
+int main(void)
+{
+	char string[] = "with proper grammar, but the outcome is a piece of art,\n";
+
+	printf(string);
+
+	return (0);
+}
+

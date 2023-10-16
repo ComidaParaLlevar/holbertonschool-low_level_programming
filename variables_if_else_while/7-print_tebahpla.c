b0a873b62f67c3b832bae4fee3b621b023ba3ed6
comidaparallevar <lyanosorio98@gmail.com>
2023-10-16 14:31:38 -0400

    added 7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - prints alphabet
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z' ; alpha <= 'a' ; alpha--)
+	{
+	putchar (alpha);
+	}
+	putchar ('\n');
+	return (0);
+}

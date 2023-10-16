0458c28d46b8eee1767ec6a7a7e7c92878bb8a7c
comidaparallevar <lyanosorio98@gmail.com>
2023-10-16 14:10:12 -0400

    added 5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - prints alphabet
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = '0' ; num <= '9' ; num++)
+	{
+	printf("%d\n", num);
+	}
+	return (0);
+}

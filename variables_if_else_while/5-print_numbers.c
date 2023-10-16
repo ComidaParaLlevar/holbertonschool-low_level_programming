cd0f490262f9f4b195a22c5785b20df248e25ffe
comidaparallevar <lyanosorio98@gmail.com>
2023-10-16 14:21:26 -0400

    ' deletion

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- *main - prints alphabet
+ *main - prints numbers from 0 to 9
  *
  *Return: 0
  */
@@ -10,9 +10,9 @@ int main(void)
 {
 	int num;
 
-	for (num = '0' ; num <= '9' ; num++)
+	for (num = 0 ; num <= 9 ; num++)
 	{
-	printf("%d", num);
+	printf("%i", num);
 	}
 	printf("\n");
 	return (0);

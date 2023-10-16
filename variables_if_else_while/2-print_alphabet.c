64b1c6ff32b3b3cc7775933cf208fde54855e267
comidaparallevar <lyanosorio98@gmail.com>
2023-10-16 13:23:46 -0400

    added 2-print_alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - prints alphabet
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for(alpha = 'a' ; alpha <= 'z' ; alpha++)
+	{
+	putchar(alpha);
+	}
+	return(0);
+}

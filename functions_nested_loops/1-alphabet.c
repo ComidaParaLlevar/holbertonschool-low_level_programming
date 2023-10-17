aa692dc24bd4f49e02fa2268f5bbbbe44641fe3b
comidaparallevar <lyanosorio98@gmail.com>
2023-10-17 14:11:06 -0400

    added 1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+# include "main.h"
+/**
+ *print_alphabet - prints alphabet
+ *Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+		_putchar(alpha);
+}

45c38c38e3f7bd4a1b0921bcd4516e1a452a19cb
comidaparallevar <lyanosorio98@gmail.com>
2023-10-17 17:56:12 -0400

    added 2-print_alphabet_x10.c test

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,21 @@
 # include "main.h"
 /**
- *print_alphabet - prints alphabet
+ *print_alphabet_x10 - prints the alphabet ten times
  *Return: 0
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	char alpha;
+	int i;
 
+for (i = 0 ; i == 10 ; i++)
+{
 	for (alpha = 'a' ; alpha <= 'z' ; alpha++)
 	{
 		_putchar(alpha);
 	}
 	_putchar('\n');
+}
 }
 

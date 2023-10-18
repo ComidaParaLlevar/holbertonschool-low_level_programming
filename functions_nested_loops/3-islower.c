38b4150139631e5b6aac8fb7cb6ffcdd108aab3d
comidaparallevar <lyanosorio98@gmail.com>
2023-10-18 09:19:38 -0400

    fixed parameter documentation 3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,8 +1,10 @@
 #include "main.h"
 /**
- *int _islower() - checks lower case
- *c - int
- *Return: 1 if true, 0 if false
+ *_islower- checks for lower case characters
+ *
+ *@c: is the character that need to be checked
+ *
+ *Return: 1 for true, 0 for false
  */
 
 int _islower(int c)
@@ -10,7 +12,7 @@ int _islower(int c)
 
 	int i;
 
-for (i = 97 ; i <=122 ; i++)
+for (i = 97 ; i <= 122 ; i++)
 {
 	if (i == c)
 	{

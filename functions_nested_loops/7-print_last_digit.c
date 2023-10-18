a462e081a7e9f305a0af036f0b0fca9c36a7b33b
comidaparallevar <lyanosorio98@gmail.com>
2023-10-18 12:45:13 -0400

    added 7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_last_digit - prints last digit of c
+ *@c: number to evaluate
+ *Return: val if last digit if number is negative, last if positive
+ */
+
+int print_last_digit(int c)
+{
+	int abs;
+	int val;
+	int last;
+
+	if (c < 0)
+	{
+		abs = c * (-1);
+		val = abs % 10;
+		return (val);
+	}
+last = c % 10;
+return (last);
+}

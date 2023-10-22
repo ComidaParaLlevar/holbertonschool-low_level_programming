4943ce1901da0492429584e93433764e10271fc6
comidaparallevar <lyanosorio98@gmail.com>
2023-10-22 01:01:14 -0400

    added 10-print_triangle.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ *print_triangle- prints square
+ *@size: size of triangle
+ *Return: void
+ */
+
+void print_triangle(int size)
+{
+	int a, b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = size; b < 1; b--)
+			{
+				if (a >= b)
+				{
+					_putchar(35);
+				}
+				else
+				{
+					_putchar(' ');
+				}
+		_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}

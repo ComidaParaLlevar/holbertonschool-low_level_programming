c107d3fdd75177164ec9ca681186f5ee3359092a
comidaparallevar <lyanosorio98@gmail.com>
2023-10-21 23:36:42 -0400

    fixed relational operator

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -11,9 +11,9 @@ void print_square(int size)
 
 	if (size > 0)
 	{
-		for (a = 0; a <= size; a++)
+		for (a = 0; a < size; a++)
 		{
-			for (b = 0; b <= size; b++)
+			for (b = 0; b < size; b++)
 			{
 				_putchar(35);
 			}

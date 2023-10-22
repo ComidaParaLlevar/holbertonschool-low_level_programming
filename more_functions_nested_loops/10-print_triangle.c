15fe65d8038b4cebb1028ef33e2d1a7df82a306b
comidaparallevar <lyanosorio98@gmail.com>
2023-10-22 09:50:50 -0400

    added 10-print_triangle.c fixed iterations of spacesx

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -11,9 +11,9 @@ void print_triangle(int size)
 
 	if (size > 0)
 	{
-		for (a = 0; a < size; a++)
+		for (a = 0; a <= size; a++)
 		{
-			for (b = 0; b < size; b++)
+			for (b = 0; b < size - a; b++)
 			{
 					_putchar(32);
 			}

59341c611e3e971c96f99e15e736b78b113d7584
comidaparallevar <lyanosorio98@gmail.com>
2023-10-22 10:14:30 -0400

    added 10-print_triangle.c put the if size < 0 at the beggining

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -9,24 +9,23 @@ void print_triangle(int size)
 {
 	int a, b, c;
 
-	If (size <= 0)
+	if (size <= 0)
 	{
 		_putchar('\n');
 		return;
 	}
 
+	for (a = 0; a <= size; a++)
 	{
-		for (a = 0; a <= size; a++)
+		for (b = 0; b < size - a; b++)
 		{
-			for (b = 0; b < size - a; b++)
-			{
-					_putchar(32);
-			}
-			for (c = 0; c < a; c++)
-			{
-				_putchar(35);
-			}
-		_putchar('\n');
+				_putchar(32);
 		}
+		for (c = 0; c < a; c++)
+		{
+			_putchar(35);
+		}
+	_putchar('\n');
 	}
 }
+

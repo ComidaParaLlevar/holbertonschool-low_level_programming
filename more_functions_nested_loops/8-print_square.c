3c0d36240ff7c4f2f699cbd23171003d955658ac
comidaparallevar <lyanosorio98@gmail.com>
2023-10-21 23:31:35 -0400

    added 8-print_square.c

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *print_square- prints square
+ *@size: size of square
+ *Return: void
+ */
+
+void print_square(int size)
+{
+	int a, b;
+
+	if (n > 0)
+	{
+		for (a = 0; a <= size; a++)
+		{
+			for (b = 0; b <= size; b++)
+			{
+				_putchar(35);
+			}
+		_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}

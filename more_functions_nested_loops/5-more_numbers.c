d16e86c725e6c2612189f01f3a8c3a9c900457e6
comidaparallevar <lyanosorio98@gmail.com>
2023-10-21 21:20:34 -0400

    added 5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *more_numbers- prints numbers 0 to 14 ten times
+ *Return: void
+ */
+
+void more_numbers(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 10; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			_putchar(b + '0');
+		}
+	_putchar('\n')
+	}
+}
+

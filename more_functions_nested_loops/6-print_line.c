8e325e6263994e0a402a59657c24216d9cff11be
comidaparallevar <lyanosorio98@gmail.com>
2023-10-21 22:02:57 -0400

    added 6-print_line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *print_line- prints underscores n times
+ *@n: provides number of times
+ *Return: void
+ */
+
+void print_line(int n)
+{
+	int a;
+
+	if (n > 0)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			_putchar(95);
+		}
+	}
+	else
+	{
+		_putchar('\n')
+	}
+	_putchar('\n');
+}
+

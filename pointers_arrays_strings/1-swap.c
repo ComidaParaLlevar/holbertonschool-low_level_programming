d5566bef31db9f0af304416fe3ed1fc8ab9a8355
comidaparallevar <lyanosorio98@gmail.com>
2023-10-23 10:08:52 -0400

    added 1-swap.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *swap_int- swaps values of variables through pointers
+ *@a: integer to be swapped
+ *@b: second integer to be swapped
+ */
+
+void swap_int(int *a, int *b);
+{
+	*a = b;
+	*b = a;
+}

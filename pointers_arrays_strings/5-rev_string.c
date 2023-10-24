dfb0218471168a0ce471683a5fdbf6994ad18d1c
comidaparallevar <lyanosorio98@gmail.com>
2023-10-24 10:14:48 -0400

    added 5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *rev_string- prints a string in reverse
+ *@s: string to print
+ */
+
+void rev_string(char *s)
+{
+	int c = 0;
+	int p = 0;
+	int hold;
+	int pt;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	c--;
+	pt = c;
+	while (p < pt)
+	{
+		hold = *(s + p);
+		*(s + p) = *(s + pt);
+		*(s + pt) = hold;
+		p++;
+		pt--
+	}
+
+}

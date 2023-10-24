beaa7feebe7f9409dc5fc6f023fe0d584cd82de5
comidaparallevar <lyanosorio98@gmail.com>
2023-10-24 14:33:28 -0400

    update #8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -10,25 +10,14 @@ void print_array(int *a, int n)
 {
 	int z = 0;
 
-	n = 0;
-
-	while (a[z])
+	while (z < n)
 	{
+		printf("%d", a[z]);
+		if (z != (n -1))
+		{
+			printf(", ");
+		}
 		z++;
-	}
-
-	while (n < (z - 1))
-	{
-		if (n == (z - 2))
-		{
-			printf("%d", a[n]);
-			n++;
-		}
-		else
-		{
-			printf("%d, ", a[n]);
-			n++;
-		}
 	}
 printf("\n");
 }

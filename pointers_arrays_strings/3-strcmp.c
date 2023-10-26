e6de47b231b7c531f6f94d61f6e45880a59f5729
comidaparallevar <lyanosorio98@gmail.com>
2023-10-26 09:57:39 -0400

    added 3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,12 +9,10 @@
 int _strcmp(char *s1, char *s2)
 {
 	int c = 0;
-	int r;
 
-	if (s1[c] == s2[c])
+	while (s1[c] && s2[c] && s1[c] == s2[c])
 	{
 		c++;
 	}
-r = s1[c] - s2[c];
-return (r);
+return (s1[c] - s2[c]);
 }

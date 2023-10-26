08952cddac9f65ce104961f72a996240c38176d2
comidaparallevar <lyanosorio98@gmail.com>
2023-10-26 09:26:21 -0400

    added 2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -4,17 +4,22 @@
  *@dest: destination of copy
  *@src: source of copy
  *@n: bytes
+ *Return: copied string
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
 	int c = 0;
 
-	while (src[c] && n > '0')
+	while (src[c] && c < n)
 	{
 		dest[c] = src[c];
 		c++;
-		n--;
 	}
-return (src);
+	while (c < n)
+	{
+		dest[c] = '\0';
+		c++;
+	}
+return (dest);
 }

08f994149d9ac3be0a9f1807e0da8bd6d0442f2c
comidaparallevar <lyanosorio98@gmail.com>
2023-10-26 08:45:00 -0400

    addd 1-strncat.c added null at end

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -12,25 +12,18 @@ char *_strncat(char *dest, char *src, int n)
 	int c = 0;
 	int d = 0;
 
-	while (n > 0 && dest[c] != '\0')
+	while (dest[c] != '\0')
 	{
 		c++;
 	}
 
-	while (n > 0 || dest[c] != '\0')
+	while (n > 0 && src[d])
 	{
 		dest[c] = src[d];
 		c++;
 		d++;
 		n--;
-		if (n <= dest[c])
-		{
-			break;
-		}
 	}
-	if (dest[c] <  n)
-	{
-		dest[c] = '\0';
-	}
+	dest[c] = '\0';
 return (dest);
 }

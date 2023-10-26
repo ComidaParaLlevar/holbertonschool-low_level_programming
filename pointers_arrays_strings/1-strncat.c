c544b471766bd41b94360b215b518147f4415ace
comidaparallevar <lyanosorio98@gmail.com>
2023-10-25 20:09:35 -0400

    addd 1-strncat.c byte limit

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -12,12 +12,12 @@ char *_strncat(char *dest, char *src, int n)
 	int c = 0;
 	int d = 0;
 
-	while (dest[c] < n && dest[c] != '\0')
+	while (dest[c] != '\0')
 	{
 		c++;
 	}
 
-	while (dest[c] != '\0')
+	while (dest[c] < n && dest[c] != '\0')
 	{
 		dest[c] = src[d];
 		c++;

327ddde91b617e381bf9e02b10612291ca43336c
comidaparallevar <lyanosorio98@gmail.com>
2023-10-25 20:27:35 -0400

    addd 1-strncat.c byte limit decrease per iteration

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -12,19 +12,19 @@ char *_strncat(char *dest, char *src, int n)
 	int c = 0;
 	int d = 0;
 
-	while (dest[c] != '\0')
+	while (n > 0 && dest[c] != '\0')
 	{
 		c++;
 	}
 
-	while (n > 0 && dest[c] != '\0')
+	while (n > 0 || dest[c] != '\0')
 	{
 		dest[c] = src[d];
 		c++;
 		d++;
 		n--;
 	}
-	if (dest[c] < n)
+	if (dest[c] >  n)
 	{
 		dest[c] = '\0';
 	}

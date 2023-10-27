9727c3d3f2b1949781cddcd05dd0e241306093d8
comidaparallevar <lyanosorio98@gmail.com>
2023-10-27 13:54:50 -0400

    added 2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -9,16 +9,17 @@
 char *_strchr(char *s, char c)
 {
 	int i = 0;
+	int d = 0;
 
 	while (s[i])
 	{
 		if (s[i] == c)
 		{
-			while (s[i])
+			while (d < i)
 			{
 				s++;
+				d++;
 			}
-			s--;
 			return (s);
 		}
 		else

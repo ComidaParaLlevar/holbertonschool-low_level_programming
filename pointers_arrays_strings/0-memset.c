93b83bb4bc7d80dc25580e639e57b7c2bc18196a
comidaparallevar <lyanosorio98@gmail.com>
2023-10-27 11:00:39 -0400

    added 0-memset.c

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_memset - fills memory with constant type
+ *@s:receiving var
+ *@b: source var
+ *@n: number of bytes
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int c = 0;
+	while (n > c)
+	{
+		s[c] = b;
+		c++;
+	}
+return (s);
+}

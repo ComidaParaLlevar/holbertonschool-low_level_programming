41627580cb9cc8a9ee421c2a4a82c82814924afa
comidaparallevar <lyanosorio98@gmail.com>
2023-11-01 16:09:27 -0400

    added 0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints name of main function
+ * @argc: number of string arguments
+ * @argv: argument vector
+ * Return: void
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}

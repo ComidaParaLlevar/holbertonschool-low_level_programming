5aa61ed687a135b95ea00d8bbb6fafe4c8c5210f
comidaparallevar <lyanosorio98@gmail.com>
2023-11-02 11:06:46 -0400

    added 2-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints all arguments
+ * @argc: number of string arguments
+ * @argv: argument vector
+ * Return: void
+ */
+
+int main(int argc, char *argv[])
+{
+	int c = 0;
+	while (argv[c])
+	{
+	printf("%s\n", argv[c]);
+	(void)argc;
+	c++;
+	}
+	return (0);
+}

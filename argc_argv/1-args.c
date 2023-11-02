2487002727e55d320c00f8be5f080eec38cedcae
comidaparallevar <lyanosorio98@gmail.com>
2023-11-02 10:06:41 -0400

    added 1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints number of arguments
+ * @argc: number of arguments passed
+ * @argv: array of arguments
+ * Return: 1 if succesful
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	(void)argv;
+	return (1);
+}

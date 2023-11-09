8a9a6971687f94bfd6a3f5e0d52d469902a28c0e
comidaparallevar <lyanosorio98@gmail.com>
2023-11-09 10:23:58 -0400

    added 1-strdup.c

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strdup - returns pointer to copy of string in parameter
+ * @str: string to duplicate
+ * Return: dup string location
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0, c = 0;
+	char *ptr = (char *)malloc((i + 1) * sizeof(char));
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	
+	while (c <= i)
+	{
+		ptr[c] = str[c];
+		c++;
+	}
+
+return (ptr);
+}
+

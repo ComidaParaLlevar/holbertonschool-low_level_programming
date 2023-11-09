1ef0bb4b23cbd9efa4bf1e24e77418486c3f3c8a
comidaparallevar <lyanosorio98@gmail.com>
2023-11-09 14:03:50 -0400

    test

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -19,10 +19,10 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[c])
+	while (s1[c] != '\0')
 		c++;
 
-	while (s2[i])
+	while (s2[i] != '\0')
 	{
 		i++;
 		c++;
@@ -34,7 +34,7 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 
 	i = 0;
-	while (s1[i] != '\0')
+	while (s1[i])
 	{
 		str[j] = s1[i];
 		j++;

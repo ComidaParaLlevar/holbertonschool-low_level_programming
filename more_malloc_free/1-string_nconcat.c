9d76f9cd9849b5c250ba6b544d34e57588286a9a
comidaparallevar <lyanosorio98@gmail.com>
2023-11-10 15:27:02 -0400

    added 1-string_nconcat.c

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,62 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: byte limit of second string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, c, l, sum;
+	char *ptr;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	while (s1[c] != '\0')
+	{
+		c++;
+	}
+
+	while (s2[l] != '\0')
+	{
+		l++;
+	}
+	if (n > l)
+	{
+		n = l;
+	}
+	sum = c + n;
+
+	ptr = malloc(sum + 1);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	l = 0;
+	for (i = 0; i <= sum; i++)
+	{
+		if (i < c)
+		{
+			ptr[i] = s1[i];
+		}
+		else
+			ptr[i] = s2[i - c];
+	}
+
+ptr[sum] = '\0';
+
+return (ptr);
+}
+
+
---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * _calloc - function that allocates memory for an array using malloc
+ * @nmemb: array to allocate
+ * @size: size of array
+ * Return: void
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+

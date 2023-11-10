4667e9ff93ee55e3e9722a44ed3c419650b64edb
comidaparallevar <lyanosorio98@gmail.com>
2023-11-09 21:56:28 -0400

    added 4-free_grid.c

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - unallocates memory of grid
+ * @grid: pointer to pointer of grid
+ * @height: rows of grid
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i ++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}

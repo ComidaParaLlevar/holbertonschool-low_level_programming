a601f8899759e51ac0bcac1d2d5fc95dedf73763
comidaparallevar <lyanosorio98@gmail.com>
2023-11-12 11:53:18 -0400

    added 3-function_like_macro.h

---
+++ preprocessor/3-function_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * defines a function-like macro to find the absolute value of x number
+ */
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+#endif

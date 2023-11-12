2b45d58f059f48d54a59216a47a65d771870a5af
comidaparallevar <lyanosorio98@gmail.com>
2023-11-12 12:05:37 -0400

    added 4-sum.h

---
+++ preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * SUM - function like macro to add two variables
+ * @x: first variable to add
+ * @y: second variable to add
+ */
+
+
+#define SUM(x, y) ((x) + (y))
+
+#endif

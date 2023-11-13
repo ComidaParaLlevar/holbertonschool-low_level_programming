c0c38c2d2d68768d1ab7a68b435bae2e0d6edffd
comidaparallevar <lyanosorio98@gmail.com>
2023-11-13 14:19:21 -0400

    added dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/*
+ * struct dog - structure to define name, age, and owner of dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};

8a28dc01559f506a1cd4b24a680258ef2c0c0512
comidaparallevar <lyanosorio98@gmail.com>
2023-11-14 14:16:36 -0400

    enclosed macro nil in ()

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -9,23 +9,21 @@
 
 void print_dog(struct dog *d)
 {
-	char *nil = nil;
-
 	if (d == 0)
 		return;
 
 	if (d->name == 0)
-		printf("Name: %s\n", nil);
+		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
 
 	if ((*d).age == 0)
-		printf("Age: %s\n", nil);
+		printf("Age: (nil)\n");
 	else
 		printf("Age: %f\n", d->age);
 
 	if (d->owner == 0)
-		printf("Owner: %s\n", nil);
+		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
 }

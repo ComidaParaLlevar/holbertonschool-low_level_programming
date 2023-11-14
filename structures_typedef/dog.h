86e8cdbd68a2dd7fd9d82dc6da8f12bbcc68d829
comidaparallevar <lyanosorio98@gmail.com>
2023-11-14 14:22:33 -0400

    new name for struct dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -9,13 +9,14 @@
  * Description: it fills the information on a pre-written
  * dentence to present a dog.
  */
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
 
 #endif

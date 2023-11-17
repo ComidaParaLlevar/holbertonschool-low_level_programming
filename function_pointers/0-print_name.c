4929fb7230ab09e8bffa28abd513554763fd0602
comidaparallevar <lyanosorio98@gmail.com>
2023-11-17 12:40:44 -0400

    added 0-print_name.c

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ * print_name - calls function to print name
+ * @name: name to print
+ * @f: function to call
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+
+return;
+}
---
+++ function_pointers/README.md
@@ -0,0 +1,1 @@
+0-print_name.c | pointer function that prints a name.

89f5b1a56b8e87d408ed06a5741ba00b64eb1519
comidaparallevar <lyanosorio98@gmail.com>
2023-11-19 10:07:20 -0400

    added va_start variable

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -14,6 +14,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *strptr;
 	unsigned int i;
 
+	va_start(str, n);
+
 	for (i = 0; i < n; i++)
 	{
 		strptr = va_arg(str, char *);

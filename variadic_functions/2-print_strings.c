717616443391becf97660ccfde6ac281abca5d5c
comidaparallevar <lyanosorio98@gmail.com>
2023-11-19 10:04:19 -0400

    added 2-print_string.c

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_strings - prints strings
+ * @separator: string to separate words
+ * @n: number of variables in function
+ * @...: variable parameter
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list str;
+	char *strptr;
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		strptr = va_arg(str, char *);
+
+		if (strptr == NULL)
+		{
+			printf("%s", "(nil)");
+		}
+		else
+			printf("%s", strptr);
+
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(str);
+}

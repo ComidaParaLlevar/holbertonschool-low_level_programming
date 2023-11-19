70aa02c7afdf179062f18d0ec43447c4fc2ab05f
comidaparallevar <lyanosorio98@gmail.com>
2023-11-18 22:11:37 -0400

    added 0-sum_them_all.c

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -9,16 +9,18 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-  va_list num;
-  unsigned int i, sum;
+	va_list num;
+	unsigned int i, sum;
 
-  va_start (num, n);
+	va_start(num, n);
 
-  sum = 0;
-  for (i = 0; i < n; i++)
-    sum += va_arg (num, int);
+	sum = 0;
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(num, int);
+	}
 
-  va_end (num);
-  return (sum);
+va_end(num);
+return (sum);
 }
 

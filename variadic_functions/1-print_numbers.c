5396578659fb7f068b2f503163505ca67a77b2fe
comidaparallevar <lyanosorio98@gmail.com>
2023-11-19 09:41:55 -0400

    added 1-print_numbers.c

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_numbers - prints numbers
+ * @separator: string separator between words
+ * @n: number of interger
+ * @...: variable parameter
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list num;
+	unsigned int i;
+
+	va_start(num, n);
+
+	for(i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(num, int));
+
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+
+	va_end(num);
+}
+
+

880a0d6c92b1fe4feff6ecb43e9709bca30063dc
comidaparallevar <lyanosorio98@gmail.com>
2023-12-08 18:34:00 -0400

    added fail cndition for open_to in while loop

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -13,14 +13,14 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	buffer = malloc(1024);
+	buffer = malloc(sizeof(char) * 024);
 	fd_from = open(argv[1], O_RDONLY);
 	fdR = read(fd_from, buffer, 1024);
-	if (fd_from == -1 || fdR == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	while (fdR > 0)
 	{
+		if (fd_from == -1 || fdR == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 		fdW = write(fd_to, buffer, fdR);
 		if (fd_to == -1 || fdW == -1 || buffer == NULL)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]), exit(99);

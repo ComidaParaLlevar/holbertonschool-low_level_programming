96c31ce2f2303ad338adc5edc6f3bd130a87e60b
comidaparallevar <lyanosorio98@gmail.com>
2023-12-08 20:32:16 -0400

    rearanged error messages

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -8,31 +8,28 @@
 
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, fdR, fdW, close_from, close_to;
-	char *buffer;
+	int fd_from, fd_to, fdR;
+	char buffer[1024];
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	buffer = malloc(1024);
 	fd_from = open(argv[1], O_RDONLY);
+	if (!argv[1] || fd_from == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	while ((fdR = read(fd_from, buffer, 1024)) > 0)
 	{
-		if (fd_from == -1 || fdR == -1)
+		if (write(fd_to, buffer, fdR) != fdR || fd_to == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
+			dprintf(STDERR_FILENO, "Error:Can't read from file %s\n", argv[1]);
+			exit(99);
 		}
-		fdW = write(fd_to, buffer, fdR);
-		if (fd_to == -1 || fdW == -1 || buffer == NULL)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]), exit(99);
 	}
-	free(buffer);
-	close_from = close(fd_from);
-	close_to = close(fd_to);
-	if (close_from == -1)
+	if (fdR == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read form file %s\n", argv[1]), exit(98);
+	if (close(fd_from) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd_from), exit(100);
-	if (close_to == -1)
+	if (close(fd_to) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd_to), exit(100);
 	return (0);
 }
